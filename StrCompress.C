f4bf0ba7b7a8f7eb19bc60c53d062312f6e5fec9
i2c <i2c@i2cs-MacBook-Pro.local>
2021-08-06 18:03:26 -0700

    first commit

---
+++ StrCompress.C
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string compress(string str) {
+
+  string newStr(str.length(),'x');
+
+  return newStr;
+}
+
+int main(int argc, char** argv) {
+
+  string s = compress("aab");
+  return 0;
+}
+  
