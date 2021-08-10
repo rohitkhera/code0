56a6c6d0a30b596ba5ff7ad5c962ac8eef914a2c
i2c <i2c@i2cs-MacBook-Pro.local>
2021-08-10 12:25:37 -0700

    bug fixes

--- StrCompress.C
+++ StrCompress.C
@@ -5,14 +5,51 @@ using namespace std;
 
 string compress(string str) {
 
-  string newStr(str.length(),'x');
+  uint32_t len = str.length();
+
+  if(str.length() <= 2)
+    return("error");
+
+  char lastchar = str[0];
+  int count = 1;
+  int index = 0;
+  string newStr(len,'x');
+
+  for(int i = 1; i < len; i++) {
+    if(lastchar == str[i]) {
+      count++;
+    }
+    else if(count > 1) {
+      newStr[index] = lastchar;
+      index++;
+      newStr[index] = count;
+      index++;
+      count = 1;
+      lastchar = str[i];
+    }
+    else {
+      newStr[index] = str[i];
+      index++;
+    }
+
+  }
 
   return newStr;
 }
 
 int main(int argc, char** argv) {
 
-  string s = compress("aab");
+  string s0 = compress("aaab"); //2ab
+  string s1 = compress("aabb"); //2a2
+  cout << s0 << endl;
   return 0;
 }
   
+/*
+
+  'aaab'
+  i = 2 lastchar = 'a'
+  i = 3 lastchar 'a', count = 3
+  
+
+ */
