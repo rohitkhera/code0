99301d710501ec6de6d5826b81c949fdd6616c63
RohitKhera <rohit.khera@ringcipher.com>
2021-08-16 15:01:43 -0700

    bug fixes

--- LongestCommonPrefix.C
+++ LongestCommonPrefix.C
@@ -8,16 +8,31 @@ using namespace std;
 
 string longestCommonPrefix(vector<string>& strs) {
 
-  string prefix;
+  string prefix = "";
   int small_len = strs[0].length();
   const int sz = strs.size();
-  for(string str: strs) {
-    if(str.length() < small_len)
-      small_len = str.length();
+
+  for(int i = 0; i < strs.size(); i++) {
+    if(strs[i].length() < small_len)
+      small_len = strs[i].length();
     
   }
-  return prefix;
 
+  bool diff = false;
+  for(int i = 0; i < small_len; i++) {
+
+    char current = strs[0][i];
+    for(int j = 0; j < sz; j++) {
+      if(strs[j][i] != current)
+	diff = true;
+    }
+    if(diff)
+      break;
+
+    prefix += current;
+  }
+  
+  return prefix;
 }
 
 
@@ -28,5 +43,6 @@ int main(int argc, char** argv) {
   strs.push_back("flower");
   strs.push_back("flow");
   strs.push_back("flight");
-
+  cout << longestCommonPrefix(strs) << endl;
+  
 }
