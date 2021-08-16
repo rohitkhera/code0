8c873d31ab5f876231177dc64daa2cdcfb97f0c2
RohitKhera <rohit.khera@ringcipher.com>
2021-08-16 11:38:44 -0700

    bug fix

--- palindromeInt.C
+++ palindromeInt.C
@@ -1,27 +1,37 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
 
-void printVec(vector<int> vec) {
+void printVec(vector<int>& vec) {
 
-  for(int i : vec)
-    cout << i << endl;
+  for(int i = 0; i < vec.size(); i++)
+    cout << vec[i] << endl;
+
 }
 
 bool is_palindrome(int x) {
 
-  vector<int> reversed;
-
-  while(x % 10) {
-    reversed.push_back(x % 10);
-    x /= 10;
+  vector<int> digits;
+  bool isPalindrome = false;
+  while(x > 0) {
+    digits.push_back(x % 10);
+    x = x/10;
   }
 
-  printVec(vec);
+  const uint32_t sz = digits.size();
+  for(int i = 0; i < sz; i++)
+    if(digits[i] != digits[sz - 1 -i])
+      return false;
+
+  return true;
+
 }
 
 
 int main(int argc, char** argv) {
 
-  cout << is_palindrome(1234);
-
+  cout << is_palindrome(12321) << endl;
+  cout << is_palindrome(3443) << endl;
+  
 }
