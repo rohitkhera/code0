82c6f547af49af538dec2335a6ccfec3bec0347d
RohitKhera <rohit.khera@ringcipher.com>
2021-08-18 15:09:01 -0700

    no mesg

--- RemoveDupsSorted.C
+++ RemoveDupsSorted.C
@@ -3,16 +3,38 @@
 
 using namespace std;
 
+// 1 1 1 2 3 
+
+void move_array(int a, int b, vector<int>& nums) {
+
+  while(b < nums.size()) {
+    nums[a+1] = nums[b];
+    a++;
+    b++;
+
+  }
+}
+
+void print(vector<int>& nums) {
+
+  for(int i = 0; i < nums.size(); i++)
+    cout << nums[i] << endl;
+  
+}
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-      int index = 0;
+      int index = 0, j = 0;
       for(int i = 0; i < nums.size() - 1; i++) {
 
-	for(int j = i; j < nums.size(); j++) {
-
+	for(j = i + 1; j < nums.size(); j++) {
+	  if(nums[j] != nums[i])
+	    break;
 	}
+	if(j > i + 1)
+	  move_array(i,j,nums);
 
       }
 
@@ -23,6 +45,21 @@ public:
 
 int main(int argc, char **argv) {
 
+
+  vector<int> nums;
+  nums.push_back(1);
+  nums.push_back(1);
+  nums.push_back(1);
+  nums.push_back(2);
+  nums.push_back(2);  
+  nums.push_back(3);
+  nums.push_back(3);
+
+  print(nums);
+  cout << endl;
+  Solution s;
+  s.removeDuplicates(nums);
+  print(nums);
   
   return 0;
 
